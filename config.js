f6692c1b28ba3194b311b8acf3f6fb25aa85f79c
poissonpasnee <ifsiapp.dental168@passmail.com>
2026-01-28 04:46:54 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,5 @@
-// config.js
-// Ce fichier contient les clés de connexion à Supabase.
-// NE LE PARTAGEZ PAS SUR GITHUB si le projet est public.
-
 window.CC_ENV = {
-    // 1. Remplacez par l'URL de votre projet (Project URL)
-    // Elle ressemble à : https://xyzxyzxyz.supabase.co
     SB_URL: "https://votre-projet.supabase.co",
-
-    // 2. Remplacez par la clé API publique (anon public key)
-    // C'est une très longue chaîne qui commence souvent par eyJhbGci...
     SB_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..." 
 };
 
