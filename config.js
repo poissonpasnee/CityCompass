e287b9e172183b07e850381ed23b73e44358dadb
poissonpasnee <ifsiapp.dental168@passmail.com>
2026-01-28 04:48:45 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.CC_ENV = {
-    SB_URL: "https://votre-projet.supabase.co",
-    SB_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..." 
+    SB_URL: "https://scbihxaceelyygrlacvm.supabase.co",
+    SB_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNjYmloeGFjZWVseXlncmxhY3ZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk1NjU3MTcsImV4cCI6MjA4NTE0MTcxN30.rgUH-kltyNX99-HTDmGEyzGyu8qFXR9LF1QxsjY0W44" 
 };
 
