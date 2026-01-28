a4491f52408488ea5d48529aa08e7801bd204af5
poissonpasnee <ifsiapp.dental168@passmail.com>
2026-01-28 02:17:01 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+const CONFIG = {
+    SUPABASE_URL: https://vjlfbcwnfwyrxstfvefq.supabase.co,
+    SUPABASE_KEY: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqbGZiY3duZnd5cnhzdGZ2ZWZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkyMDc2MjcsImV4cCI6MjA4NDc4MzYyN30.WTuWbxB-8isJOp2lNnq73njQmifDjxTNEWngMgKtmv4
+};
