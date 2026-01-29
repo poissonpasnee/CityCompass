deff893cbc31b6779d76d5f8b624ebea87b61bf9
poissonpasnee <ifsiapp.dental168@passmail.com>
2026-01-29 17:16:38 +0100

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,15 @@
- 
+const UI = {
+    msg: (t) => document.getElementById('msg').innerText = t,
+    show: (viewId) => {
+        document.querySelectorAll('.view').forEach(v => v.classList.remove('active'));
+        document.getElementById(viewId).classList.add('active');
+        
+        // Gérer la navbar
+        const nav = document.getElementById('navBar');
+        if(viewId === 'viewLogin' || viewId === 'viewCam') nav.style.display = 'none';
+        else nav.style.display = 'flex';
+        
+        // Resize Map
+        if(viewId === 'viewDiscover' && AppState.map) setTimeout(() => AppState.map.invalidateSize(), 300);
+    }
+};
